593a84560de19cffd54008f69a978eac16581027
IvanGMtz <navi.garces24@gmail.com>
2023-10-04 16:05:11 -0500

    feat: :sparkles: Update routes for components as costumer

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -1,12 +1,30 @@
 import { lazy } from 'react';
 
 const Profile = lazy(() => import('../pages/ProfilePage'));
+const BooksPage = lazy(() => import('../pages/BooksPage'));
+const LoansPage = lazy(() => import('../pages/LoansPage'));
+const MakeLoansPage = lazy(() => import('../pages/MakeLoansPage'));
 
 const coreRoutes = [
   {
     path: '/profile',
     title: 'Profile',
     component: Profile,
+  },
+  {
+    path: '/books',
+    title: 'Books',
+    component: BooksPage,
+  },
+  {
+    path: '/loans',
+    title: 'Loans',
+    component: LoansPage,
+  },
+  {
+    path: '/makeLoans',
+    title: 'MakeLoans',
+    component: MakeLoansPage,
   },
 ];
 
