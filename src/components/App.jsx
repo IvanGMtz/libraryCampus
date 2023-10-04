ce3814a3ab99833134341ca86536aee3db0eea21
IvanGMtz <navi.garces24@gmail.com>
2023-10-04 03:26:13 -0500

    feat: :sparkles: Update app.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AuthProvider } from "./shared/context/AuthContext";
+import Error404 from "./pages/Error404";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import ProtectedRouted from "./shared/ProtectedRouted";
+import SignIn from "./pages/Authentication/SignIn";
+import SignUp from "./pages/Authentication/SignUp";
+
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Hola mundo</h1>
-      </div>
-    </>
-  )
+    <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="*" element={<Error404 />} />
+
+            <Route element={<ProtectedRouted />}>
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App
