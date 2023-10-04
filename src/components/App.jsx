3e754b510f6022da0a68113a8c1c0ea7a167c8a6
IvanGMtz <navi.garces24@gmail.com>
2023-10-04 12:47:20 -0500

    feat: :sparkles: Update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,53 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { AuthProvider } from "./shared/context/AuthContext";
 import Error404 from "./pages/Error404";
 import HomePage from "./pages/HomePage";
-import ProfilePage from "./pages/ProfilePage";
 import ProtectedRouted from "./shared/ProtectedRouted";
 import SignIn from "./pages/Authentication/SignIn";
 import SignUp from "./pages/Authentication/SignUp";
+import Loader from './common/Loader';
+import routes from './routes';
+import { Suspense, useEffect, useState } from "react";
+import SignUpEmployee from "./pages/Authentication/SignUpEmployee";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  return (
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <> 
     <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/register" element={<SignUp />} />
-            <Route path="*" element={<Error404 />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<SignIn />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/registerE" element={<SignUpEmployee />} />
+          <Route path="*" element={<Error404 />} />
 
-            <Route element={<ProtectedRouted />}>
-              <Route path="/profile" element={<ProfilePage />} />
+          <Route element={<ProtectedRouted />}>
+            <Route element={<HomePage />}>
+              {routes.map(({ path, component: Component }) => (
+                <Route
+                  path={path}
+                  key={path}
+                  element={
+                    <Suspense fallback={<Loader />}>
+                      <Component />
+                    </Suspense>
+                  }
+                />
+              ))}
             </Route>
-          </Routes>
-        </BrowserRouter>
-    </AuthProvider>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
 
