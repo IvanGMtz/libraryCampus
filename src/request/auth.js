12c82878b929ee47e36411d86c4d31ecbb4837ca
IvanGMtz <navi.garces24@gmail.com>
2023-10-04 12:41:40 -0500

    feat: :sparkles: Add new request to register the employees

--- src/request/auth.js
+++ src/request/auth.js
@@ -1,6 +1,8 @@
 import axios from "./axios";
 
-export const registerRequest = user => axios.post(`/auth/register`, user)
+export const registerRequest = user => axios.post(`/auth/register`, user);
+
+export const registerRequestEmployee = user => axios.post(`/auth/registerEmp`, user)
 
 export const loginRequest = user => axios.post(`/auth/login`, user);
 
