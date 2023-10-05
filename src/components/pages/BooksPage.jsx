a7e72f88529d301f2e6331a7c05e375d73a1fb10
IvanGMtz <navi.garces24@gmail.com>
2023-10-05 05:28:38 -0500

    feat: :sparkles: Update books page

--- src/components/pages/BooksPage.jsx
+++ src/components/pages/BooksPage.jsx
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import { useBooks } from "../shared/context/BookContext"
+import BookCard from "../shared/BookCard";
+
 function BooksPage() {
+
+  const { getBooks, books } = useBooks();
+
+  useEffect(() => {
+    getBooks()
+  }, []);
+
   return (
-    <div>BooksPage</div>
-  )
+    <>
+      {books.length === 0 && (
+        <div className="flex justify-center items-center p-10">
+          <div>
+            <h1 className="font-bold text-xl">
+              No Books yet, please add a new Book
+            </h1>
+          </div>
+        </div>
+      )}
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
+        {
+          books.map((book, index) => (
+            <BookCard book={book} key={book._id} index={index} />
+          ))
+        }
+      </div>
+    </>
+  );
 }
 
 export default BooksPage
