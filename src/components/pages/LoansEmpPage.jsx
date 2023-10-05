04314dc23b96a682fce0dfc431970394cd8f085e
IvanGMtz <navi.garces24@gmail.com>
2023-10-05 12:14:31 -0500

    feat: :sparkles: Update loans page for employees

--- src/components/pages/LoansEmpPage.jsx
+++ src/components/pages/LoansEmpPage.jsx
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import { useLoans } from "../shared/context/LoanContext"
+import LoanCard from "../shared/LoanCard";
+
 function LoansEmpPage() {
+
+  const { getLoansEmp, LoansEmp } = useLoans();
+
+  useEffect(() => {
+    getLoansEmp()
+  }, []);
+
   return (
-    <div>LoansEmpPage</div>
-  )
+    <>
+      {LoansEmp.length === 0 && (
+        <div className="flex justify-center items-center p-10">
+          <div>
+            <h1 className="font-bold text-xl">
+              No Loans yet, please add a new Loan
+            </h1>
+          </div>
+        </div>
+      )}
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
+        {
+          LoansEmp.map((Loan, index) => (
+            <LoanCard Loan={Loan} key={Loan._id} index={index} />
+          ))
+        }
+      </div>
+    </>
+  );
 }
 
 export default LoansEmpPage
